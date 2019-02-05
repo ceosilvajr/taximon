c72730f691a4861d8892afbbbb59962c412132d0
Ariel Silva <ceosilvajr@gmail.com>
2019-02-05 18:21:26 +0800

    - added new fragments for list and map - added map and list icon - added bottom navigation - added navigation graph

--- app/src/main/java/com/ceosilvajr/mytaxi/MainActivity.kt
+++ app/src/main/java/com/ceosilvajr/mytaxi/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.ceosilvajr.mytaxi
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
+/**
+ * @author ceosilvajr@gmail.com
+ */
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setSupportActionBar(toolbar)
+        val host: NavHostFragment = supportFragmentManager
+                .findFragmentById(R.id.my_nav_host_fragment) as NavHostFragment? ?: return
+        navController = host.navController
+        setupBottomNavMenu(navController)
     }
+
+    private fun setupBottomNavMenu(navController: NavController) {
+        bottom_nav_view.setupWithNavController(navController)
+    }
+
 }
